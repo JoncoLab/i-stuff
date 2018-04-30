56dd1b9f0932cd9d52a94806b576c2f8d3f5dcf2
princeShozanah <max.pechko@gmail.com>
2018-04-30 17:37:07 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import './index.css'
+import registerServiceWorker from './registerServiceWorker'
+import * as $ from "jquery"
+import * as M from "materialize-css"
+import "materialize-css/dist/css/materialize.css"
+import i18n from "i18next"
+import languageDetector from "i18next-browser-languagedetector"
+import Backend from "i18next-xhr-backend"
+import {reactI18nextModule} from "react-i18next"
+
+$(document).ready(() => {
+    M.Parallax.init($(".parallax"));
+    M.Sidenav.init($(".sidenav-trigger"));
+    M.Carousel.init($(".carousel"), {
+        fullWidth: false,
+        padding: 100,
+        dist: -20,
+        numVisible: 5,
+        noWrap: false
+    });
+});
+
+i18n
+    .use(Backend)
+    .use(languageDetector)
+    .use(reactI18nextModule)
+    .init({
+        callbackLng: 'ru',
+        ns: ['translations'],
+        defaultNs: 'translations',
+        debug: true,
+        interpolation: {
+            escapeValue: false
+        },
+        react: {
+            wait: true
+        }
+    });
+
+ registerServiceWorker();
