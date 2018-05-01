d4ffdb7683ce1a6f5a3f918bc600e416f7fe2f23
Князь В'Ахуї <nem97.sv@gmail.com>
2018-05-01 20:16:23 +0300

    fiiiiiiiiiiiiix

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import registerServiceWorker from './registerServiceWorker'
 import * as $ from "jquery"
 import * as M from "materialize-css"
 import "materialize-css/dist/css/materialize.css"
-import i18n from "i18next"
-import languageDetector from "i18next-browser-languagedetector"
-import Backend from "i18next-xhr-backend"
-import {reactI18nextModule} from "react-i18next"
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
@@ -22,23 +18,6 @@ $(document).ready(() => {
         noWrap: false
     });
 });
-
-i18n
-    .use(Backend)
-    .use(languageDetector)
-    .use(reactI18nextModule)
-    .init({
-        callbackLng: 'ru',
-        ns: ['translations'],
-        defaultNs: 'translations',
-        debug: true,
-        interpolation: {
-            escapeValue: false
-        },
-        react: {
-            wait: true
-        }
-    });
 
  registerServiceWorker();
 
